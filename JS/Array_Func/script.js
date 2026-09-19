18c5445d1a95c576a3ff7c7599bd8544f532ea6f
Omartech312 <sor22@nau.edu>
2026-09-18 19:03:13 -0700

    Separate rest and spread operators from array methods

--- JS/Array_Func/script.js
+++ JS/Array_Func/script.js
@@ -3,26 +3,34 @@ function Rest(){
     let user1 = document.getElementById("user1I").value;
     let user2 = document.getElementById("user2I").value;
     let user3 = document.getElementById("user3I").value;    
-    
-    forE(user1, user2, user3);
+    if(user1 && user2 && user3){
+        spread(user1, user2, user3);
+    }
+    else{
+        document.getElementById("spreadOut").textContent = "Make sure you provide an input for all three users";
+    }
 }
 
 // Rest operator compacts the different users into an array values
-function forE(...values){
+function spread(...values){
 
     // Spread operator now unpacks the users and displays them
-    document.getElementById("spreadOut").textContent = values;
-    
-    let eaOut = document.getElementById("eachOut");
-    eaOut.innerHTML = "";
-    console.log(values);
+    for(let i = 0; i < 3; i++){
+        values[i] = values[i][0].toUpperCase() + values[i].slice(1);
+        if(i > 0){
 
-    values.forEach(function display(item, index, array){
-        if(item.length > 0) eaOut.innerHTML += `- User #${index+1}: ${item}<br>`;
-        else{
-            eaOut.innerHTML += `- User #${index+1}: Does not exist, Please provide a proper name <br>`;
         }
-        array[index][0] = array[index].charAt(0).toUpperCase();
-    });
-    console.log(...values);
+    }
+
+    display(...values);
+}
+
+function display(u1, u2, u3){
+    document.getElementById("spreadOut").innerHTML =  `User1: ${u1} <br>User2: ${u2}<br>User3: ${u3}`;
+}
+
+function each(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
 }
