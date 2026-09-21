fdb643fd15a433b595c65f884f68680bbab9d8e1
Omartech312 <sor22@nau.edu>
2026-09-21 15:56:55 -0700

    object section is now ready for use, moving on to classes

--- JS/Func_Class/scripts.js
+++ JS/Func_Class/scripts.js
@@ -1,4 +1,4 @@
-console.log("working");
+//console.log("working");
 
 // Function Expression is a way to define functions as values or variables. Which I used in the previous section: Rest & Spread Syntax and Array Methods
 // instead of defining a whole function I wrote the code for a function directly the array methods
@@ -22,3 +22,31 @@ expression();
 // that are only used once (parameters) => some logic
 let print = () => document.getElementById("arrOutput").textContent = "This text was printed using an arrow function";
 print();
+
+
+//object is a collection of properties and methods representing real life objects in a sense
+const learner = {
+    name: "Omar",
+    last: "Salgado",
+    age: 22,
+    activity: "Learning",
+    greeting: function(){ document.getElementById("myObjOut").textContent = `Hello, My name is ${learner.name} ${learner.last}`; },
+    askAge: function(){ document.getElementById("myObjOut").textContent = `I'm ${learner.age} and always willing to ${learner.activity}`;}
+};
+
+// The fact that an object may contain some data doesnt necessary mean that you must use them. However, in most cases you want to only store important data.
+// Otherwise, you'll only waste resources.
+const hero = {
+    name: "Bruce",
+    last: "Wayne",
+    age: 35,
+    activity: "Protecting Gotham",
+    greeting: function(){document.getElementById("heroObjOut").textContent = `I'm Batman`;},
+    secret: function(){console.log(`My real identity is ${this.name} ${this.last} and my duty is ${this.activity}`);}
+};
+
+document.getElementById("Igreet").addEventListener("click", learner.greeting);
+document.getElementById("Iage").addEventListener("click", learner.askAge);
+
+document.getElementById("hgreet").addEventListener("click", hero.greeting);
+hero.secret();
