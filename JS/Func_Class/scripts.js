78be8711425a8b0de0f41f2577a8e4aedd756ec3
Omartech312 <sor22@nau.edu>
2026-09-20 18:22:31 -0700

    initial functional test for arrow functions

--- JS/Func_Class/scripts.js
+++ JS/Func_Class/scripts.js
@@ -3,6 +3,9 @@ console.log("working");
 // Function Expression is a way to define functions as values or variables. Which I used in the previous section: Rest & Spread Syntax and Array Methods
 // instead of defining a whole function I wrote the code for a function directly the array methods
 
+// Function expressions are commonly used in callbacks for asynchronous operations
+// Higher-Order Functions, Closures, Event Listners
+
 function expression(){
     let arr = ["Majin Boo", "Son Gohan", "Son Goku"];
     let expOut = document.getElementById("funcExp");
@@ -11,5 +14,10 @@ function expression(){
         expOut.innerHTML += `${index+1}: ${character}<br>`;
     });
 }
-
 expression();
+
+
+let print = () => console.log("test");
+
+//arrow functions are a concise way to write function expressions. A good fit for simple functions
+// that are only used once (parameters) => some logic
